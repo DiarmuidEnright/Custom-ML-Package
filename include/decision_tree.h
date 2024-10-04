5ae89bab1f01c97e490f448b93b27022d1ab577f
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-04 23:42:38 +0100

    More...

--- include/decision_tree.h
+++ include/decision_tree.h
@@ -1,20 +1,16 @@
 #ifndef DECISION_TREE_H
 #define DECISION_TREE_H
 
-typedef struct TreeNode {
-    double threshold;
-    int feature_index;
-    double value;
-    struct TreeNode *left;
-    struct TreeNode *right;
-} TreeNode;
+#include <stddef.h>
+
+typedef struct TreeNode TreeNode;
 
 typedef struct {
     TreeNode *root;
 } DecisionTree;
 
 DecisionTree* decision_tree_train(double **X, double *y, size_t n_samples, size_t n_features, size_t max_depth, size_t min_samples_split);
-double decision_tree_predict(TreeNode *node, double *x);  // Add this
-void decision_tree_free(DecisionTree *tree);              // Add this
+double decision_tree_predict(TreeNode *node, double *x);
+void decision_tree_free(DecisionTree *tree);
 
 #endif
---
+++ src/random_forest.c
@@ -0,0 +1,46 @@
+#include "random_forest.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+RandomForest* random_forest_train(double **X, double *y, size_t n_samples, size_t n_features, size_t n_trees, size_t max_depth, size_t min_samples_split) {
+    RandomForest *forest = (RandomForest *)malloc(sizeof(RandomForest));
+    forest->n_trees = n_trees;
+    forest->trees = (DecisionTree **)malloc(n_trees * sizeof(DecisionTree *));
+
+    for (size_t i = 0; i < n_trees; i++) {
+        double **bootstrap_X = (double **)malloc(n_samples * sizeof(double *));
+        double *bootstrap_y = (double *)malloc(n_samples * sizeof(double));
+
+        for (size_t j = 0; j < n_samples; j++) {
+            size_t idx = rand() % n_samples;
+            bootstrap_X[j] = X[idx];
+            bootstrap_y[j] = y[idx];
+        }
+
+        forest->trees[i] = decision_tree_train(bootstrap_X, bootstrap_y, n_samples, n_features, max_depth, min_samples_split);
+
+        free(bootstrap_X);
+        free(bootstrap_y);
+    }
+
+    return forest;
+}
+
+double random_forest_predict(RandomForest *forest, double *x) {
+    size_t votes[2] = {0, 0};
+
+    for (size_t i = 0; i < forest->n_trees; i++) {
+        double prediction = decision_tree_predict(forest->trees[i]->root, x);
+        votes[(int)prediction]++;
+    }
+
+    return (votes[0] > votes[1]) ? 0 : 1;
+}
+
+void random_forest_free(RandomForest *forest) {
+    for (size_t i = 0; i < forest->n_trees; i++) {
+        decision_tree_free(forest->trees[i]);
+    }
+    free(forest->trees);
+    free(forest);
+}
