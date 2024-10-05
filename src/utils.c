b0a25015d61e69c392cb14498b93877a7100895e
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-05 21:18:28 +0100

    properly implemented ultils.c to normalize data

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,28 @@
 #include "utils.h"
+#include <stdlib.h>
 #include <math.h>
+#include <stddef.h>
 
 void normalize_data(double **X, size_t n_samples, size_t n_features) {
-    // Implement min-max normalization or z-score normalization
+    for (size_t i = 0; i < n_features; i++) {
+        double sum = 0.0;
+        for (size_t j = 0; j < n_samples; j++) {
+            sum += X[j][i];
+        }
+        double mean = sum / n_samples;
+
+        double sum_squared_diff = 0.0;
+        for (size_t j = 0; j < n_samples; j++) {
+            sum_squared_diff += (X[j][i] - mean) * (X[j][i] - mean);
+        }
+        double stddev = sqrt(sum_squared_diff / n_samples);
+
+        for (size_t j = 0; j < n_samples; j++) {
+            if (stddev != 0) {
+                X[j][i] = (X[j][i] - mean) / stddev;
+            } else {
+                X[j][i] = 0;
+            }
+        }
+    }
 }
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <stddef.h>
 
 void normalize_data(double **X, size_t n_samples, size_t n_features);
 
