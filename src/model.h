5c7b972bd74f03f0c02ef8cb4ad4ab1392707bd2
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 16:49:33 +0100

    :3

--- src/model.h
+++ src/model.h
@@ -3,11 +3,10 @@
 
 #include "decision_tree.h"
 
-typedef struct Model {
-    void (*train)(struct Model *self, double **X, double *y, int n_samples, int n_features, size_t max_depth, size_t min_samples_split);
-    double (*predict)(struct Model *self, double *data, int n_features);
-    void (*free)(struct Model *self);
-    DecisionTree *current_tree;
+typedef struct {
+    void (*train)(Model *self, double **data, double *target, int n_samples, int n_features);
+    double (*predict)(Model *self, double *data, int n_features);
+    void (*free)(Model *self);
 } Model;
 
 double model_evaluate(Model *model, double **data, int n_samples, int n_features);
