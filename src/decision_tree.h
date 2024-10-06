6eef842aa4850c4b7a3a78554f6b7361668b58f7
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 16:27:13 +0100

    updated decision_tree.h

--- src/decision_tree.h
+++ src/decision_tree.h
@@ -17,6 +17,8 @@ typedef struct DecisionTree {
     TreeNode *root;
 } DecisionTree;
 
+Model* create_decision_tree(void);
+
 void decision_tree_train(Model *self, double **X, double *y, int n_samples, int n_features, size_t max_depth, size_t min_samples_split);
 void decision_tree_free(DecisionTree *tree);
 double decision_tree_predict(TreeNode *node, double *x);
