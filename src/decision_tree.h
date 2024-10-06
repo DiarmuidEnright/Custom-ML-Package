f193e4469a2ff23b0cf3cc0eb36bff6b8a199dc5
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 21:00:20 +0100

    removed function bodies from .h

--- src/decision_tree.h
+++ src/decision_tree.h
@@ -1,12 +1,9 @@
 #ifndef DECISION_TREE_H
 #define DECISION_TREE_H
 
-
 #include <stdlib.h>
 
-
 typedef struct DecisionTree DecisionTree;
-
 
 typedef struct Model {
     DecisionTree *current_tree;
@@ -14,7 +11,6 @@ typedef struct Model {
     double (*predict)(struct Model *self, double *data, int n_features);
     void (*free)(struct Model *self);
 } Model;
-
 
 typedef struct TreeNode {
     size_t feature_idx;
@@ -24,25 +20,16 @@ typedef struct TreeNode {
     struct TreeNode *right;
 } TreeNode;
 
-
 typedef struct DecisionTree {
     TreeNode *root;
 } DecisionTree;
 
-
 Model* create_decision_tree(void);
 
+void decision_tree_train(Model *self, double **X, double *y, int n_samples, int n_features, size_t max_depth, size_t min_samples_split);
 
-void decision_tree_train(Model *self, double **X, double *y, int n_samples, int n_features, size_t max_depth, size_t min_samples_split) {
-}
+void decision_tree_free(DecisionTree *tree);
 
-
-void decision_tree_free(DecisionTree *tree) {
-}
-
-
-double decision_tree_predict(TreeNode *node, double *x) {
-}
-
+double decision_tree_predict(TreeNode *node, double *x);
 
 #endif
