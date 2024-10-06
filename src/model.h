ea9d72fe1f184ef7367ba3e305c4ec15aac12525
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 17:34:23 +0100

    :3

--- src/model.h
+++ src/model.h
@@ -4,10 +4,12 @@
 #include "decision_tree.h"
 
 double model_evaluate(Model *model, double **data, int n_samples, int n_features);
+
 Model* create_model();
 Model* create_knn();
 Model* create_svm();
 Model* create_placeholder_model();
+
 void free_model(Model *model);
 
 #endif
