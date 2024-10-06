7a96d83ae001f288a3bd96a991ba211c10132afb
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 13:44:58 +0100

    defined dataset call in dataset.h

--- src/dataset.h
+++ src/dataset.h
@@ -3,6 +3,13 @@
 
 #include <stddef.h>
 
+typedef struct {
+    double **X;
+    double *y;
+    size_t n_samples;
+    size_t n_features;
+} Dataset;
+
 double** load_dataset(const char *filename, size_t *n_samples, size_t *n_features);
 void split_dataset(double **X, double *y, size_t n_samples, double train_ratio, double ***X_train, double ***X_test, double **y_train, double **y_test);
 void k_fold_split(double **X, double *y, size_t n_samples, size_t k, size_t fold_idx, double ***X_train, double ***X_val, double **y_train, double **y_val);
