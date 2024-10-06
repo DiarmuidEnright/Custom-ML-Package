7427c415e36a03dd1ce27345f00dc24ffdc53fe4
Diarmuid Anthony Enright <diarmuidenright29@gmail.com>
2024-10-06 21:45:05 +0100

    :3

--- src/decision_tree.h
+++ src/decision_tree.h
@@ -27,9 +27,8 @@ typedef struct Model {
 
 Model* create_decision_tree(void);
 TreeNode* decision_tree_create(double **X, double *y, int n_samples, int n_features, size_t depth, size_t max_depth, size_t min_samples_split);
-TreeNode* create_majority_class(double *y, int n_samples);
 TreeNode* create_leaf_node(double value);
-
+TreeNode* create_majority_class(double *y, int n_samples);
 
 void decision_tree_train(Model *self, double **X, double *y, int n_samples, int n_features, size_t max_depth, size_t min_samples_split);
 
